0659e882795bf4fdee90ba7ecdce2315e5e6e527
Codeur16 <charlesnjiosseu@gmail.com>
2023-11-12 04:53:40 +0100

    up date

---
+++ moderation/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    // if comment has mouf change status to rejected
+    const status = data.content.includes('mouf') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content
+      }
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(4003, () => {
+  console.log('Listening on 4003');
+});
