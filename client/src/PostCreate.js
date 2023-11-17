3881eb28c0a764c99ce80c52d082f8e259674e6a
Codeur16 <charlesnjiosseu@gmail.com>
2023-11-17 03:53:35 +0100

    NodePort architecture is done

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post("http://localhost:4000/posts", {
+    await axios.post("http://192.168.49.2:31607/posts", {
       title,
     });
 
